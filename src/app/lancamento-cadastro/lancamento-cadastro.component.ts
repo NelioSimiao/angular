ec7406ea27709661148fc0251f2412147e968b3d
nmuchisse <neliosimiao@hotmail.com>
2019-03-03 23:37:33 +0200

    untracked file

---
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' },
+  ];
+
+  categorias = [
+    { label: 'Alimetacao', value: 1 },
+    { label: 'Transporte', value: 2 },
+  ];
+
+  pessoas = [
+    { label: 'Nelio Simiao Muchisse', value: 1 },
+    { label: 'Mateus joao Mateu', value: 2 },
+    { label: 'Maria salolo mmas', value: 3},
+
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
